65671d7a4bb394498816a3edae63986fcd4674a7
morimotodev93 <bestlife.morimoto@gmail.com>
2026-09-21 11:01:07 +0900

    chore(lint): allow intentionally unused parameters

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,18 @@ import { defineConfig, globalIgnores } from "eslint/config";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
